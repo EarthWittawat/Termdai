ddfc6d1b53a33567b0f51ffd1b93e11998558b70
earthwittawat2548 <earthwittawat2005@gmail.com>
2023-03-08 22:47:01 +0700

    commit

--- pages/แบบฝึกหัด.py
+++ pages/แบบฝึกหัด.py
@@ -7,15 +7,9 @@ import os
 import os.path
 from google.oauth2 import service_account
 from google.cloud import storage
+from fastbook import *
+import torch
 
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = storage.Client(credentials=credentials)
-
-bucket_name = "termdai-bucket"
-bucket = client.bucket(bucket_name)
 st.set_page_config(
     page_title="Termdai",
     layout="wide",
@@ -28,20 +22,4 @@ with st.sidebar:
         "ติดต่อ\nFacebook: Wittawat Kitipatthavorn\nTel: 086-398-1093\nLine: wittawat_earth"
     )
 with st.container():
-    st.header("รายการชุดข้อมูลแบบฝึกเพลงไทยเดิม")
-    type = st.selectbox("หัวข้อแบบฝึกหัด", ["การเปล่งเสียงกลุ่มคำ", "กลวิธีขับร้อง"])
-    path = "audio/" + type
-    ex1_list = ['เออ','อี','เอย','เอ๋ย']
-    ex2_list = ['การกลิ้งเสียง', 'การเกลือกเสียง']
-    # if os.path.exists(path):
-    if type == "การเปล่งเสียงกลุ่มคำ":
-        for audio in ex1_list:
-            st.subheader(audio)
-            blobs = bucket.list_blobs(prefix=os.path.join(path,audio))
-            blobs_full = [file.name for file in blobs]
-            blobs_cut = [file.replace(os.path.join(path,audio)+'/', '') for file in blobs_full]
-            for audio_list , audio_file in zip(blobs_cut, blobs_full):
-                st.write(audio_list)
-                blob = bucket.blob(audio_file)
-                with blob.open("rb") as f:
-                    st.audio(f.read(), format="audio/wav")      
+    audio = audiorecorder("Click to record", "กำลังบันทึกเสียง")
