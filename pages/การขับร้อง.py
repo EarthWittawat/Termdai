58ced1549fca12dd6ccb40b81732dd620a30deb3
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-04-01 01:53:52 +0700

    Update การขับร้อง.py

--- pages/การขับร้อง.py
+++ pages/การขับร้อง.py
@@ -23,15 +23,17 @@ with st.container():
     with col1:
         st.subheader('ฝึกขับร้องเพลงไทยเดิม')
         st.markdown("***")
-    
+    option = st.selectbox(
+    'เลือกเพลงที่ต้องการฝึก',
+    ('ต้นเพลงฉิ่ง 3 ชั้น', 'แขกวรเทศ'))
+    st.header(option)
     file = st.select_slider(
         'เลือกท่อนที่ต้องการฝึกร้อง',
         options=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'])
+    
     # audio_file = open('../audio/ขับร้องเพลงไทยเดิม/'+ color +'.wav', 'rb')
     path = "audio/ขับร้องเพลงไทยเดิม/" + file + '.wav'
     blob = bucket.blob(path)
     with blob.open("rb") as f:
         st.audio(f.read(), format="audio/wav")   
-    option = st.selectbox(
-    'เลือกเพลงที่ต้องการฝึก',
-    ('ต้นเพลงฉิ่ง 3 ชั้น', 'แขกวรเทศ'))
+    st.markdown("***")
