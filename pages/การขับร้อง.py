64861f26431e77c7641820da00120dd452817bf6
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-04-01 01:22:04 +0700

    Update การขับร้อง.py

--- pages/การขับร้อง.py
+++ pages/การขับร้อง.py
@@ -4,7 +4,8 @@ color = st.select_slider(
     'Select a color of the rainbow',
     options=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'])
 
-audio_file = open('../audio/ขับร้องเพลงไทยเดิม/'+ color +'.wav', 'rb')
+# audio_file = open('../audio/ขับร้องเพลงไทยเดิม/'+ color +'.wav', 'rb')
+st.write('../audio/ขับร้องเพลงไทยเดิม/'+ color +'.wav')
 audio_bytes = audio_file.read()
 
 st.audio(audio_bytes, format='audio/wav')
