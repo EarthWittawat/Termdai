9c1827ea3babe87abd88ceab85aaecfba1cec7d1
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-04-01 01:17:28 +0700

    Update การขับร้อง.py

--- pages/การขับร้อง.py
+++ pages/การขับร้อง.py
@@ -3,4 +3,4 @@ import streamlit as st
 color = st.select_slider(
     'Select a color of the rainbow',
     options=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'])
-st.write('My favorite color is', color)
+st.audio('./audio/ขับร้องเพลงไทยเดิม/'+ color)
