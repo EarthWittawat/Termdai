2ede726a6ddc8719e2ee8b711df875f5b389315e
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-04-01 01:08:45 +0700

    Update การขับร้อง.py

--- pages/การขับร้อง.py
+++ pages/การขับร้อง.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
-start_color, end_color = st.select_slider(
-    'Select a range of color wavelength',
-    options=['1', '2', '3', '4', '5', '6', '7'],
-    value=('red', 'blue'))
-st.write('You selected wavelengths between', start_color, 'and', end_color)
+color = st.select_slider(
+    'Select a color of the rainbow',
+    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+st.write('My favorite color is', color)
